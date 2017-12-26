71021217afca86f42443e3557785dd8e7125cb25
Richard Hoogstraaten <richard@hoogstraaten.eu>
2017-12-26 01:18:21 +0100

    Promises are awesome

--- rgb.js
+++ rgb.js
@@ -49,71 +49,90 @@ const oauth_data = {
     "scope": process.env.SCOPE_INT
 };
 
-function getApiTokens () {
-    axios.post(
-        'https://hoogstraaten.eu/oauth/token', oauth_data, {
-            headers: {
-                'content-type': 'application/json'
-            }
-        })
-        .then(function (response) {
-            api_tokens = response.data;
-            api_tokens.renew_on = moment().add(response.data.expires_in, 'seconds');
-        })
-        .catch(function (error) {
-            console.log(error);
-        });
-}
-
-function refreshApiTokens () {
-    axios.post(
-        'https://hoogstraaten.eu/oauth/token', {
-            "grant_type": 'refresh_token',
-            "refresh_token": api_tokens.refresh_token,
-            "client_id": process.env.CLIENT_ID,
-            "client_secret": process.env.CLIENT_SECRET,
-            "scope": process.env.SCOPE_INT
-        },
-        {
-            headers: {
-                'content-type': 'application/json'
-            }
-        })
-        .then(function (response) {
-            api_tokens = response;
-            api_tokens.renew_on = moment().add(response.expires_in, 'seconds');
-        })
-        .catch(function (error) {
-            console.log(error);
-        });
-}
-
 function api_call(url, method, data) {
-    if (api_tokens == null) {
-        getApiTokens();
-        console.log(api_tokens);
-    } else if (api_tokens.renew_on < moment()) {
-        refreshApiTokens();
-        console.log(api_tokens);
-    } else {
-        axios({
-            method: method,
-            url: url,
-            data: data,
-            headers: {
-                'authorisation': 'Bearer '.concat(api_tokens.access_token),
-                'content-type': 'application/json'
-            }
-        })
-        .then(function(response) {
-            console.log(response.data);
-        });
-    }
+    return new Promise(function(resolve, reject) {
+        if (api_tokens === null) {
+            axios.post(
+                'https://hoogstraaten.eu/oauth/token', oauth_data, {
+                    headers: {
+                        'content-type': 'application/json'
+                    }
+                })
+                .then(function (response) {
+                    api_tokens = response.data;
+                    api_tokens.renew_on = moment().add(response.data.expires_in, 'seconds');
+
+                    axios({
+                        method: method,
+                        url: url,
+                        data: data,
+                        headers: {
+                            'authorization': 'Bearer '.concat(api_tokens.access_token),
+                            'content-type': 'application/json'
+                        }
+                    }).then(function (response) {
+                        resolve(response.data);
+                    }).catch(function (error) {
+                        reject(error);
+                    });
+                })
+                .catch(function (error) {
+                    reject(error);
+                });
+        } else if (api_tokens.renew_on < moment()) {
+            axios.post(
+                'https://hoogstraaten.eu/oauth/token', {
+                    "grant_type": 'refresh_token',
+                    "refresh_token": api_tokens.refresh_token,
+                    "client_id": process.env.CLIENT_ID,
+                    "client_secret": process.env.CLIENT_SECRET,
+                    "scope": process.env.SCOPE_INT
+                },
+                {
+                    headers: {
+                        'content-type': 'application/json'
+                    }
+                })
+                .then(function (response) {
+                    api_tokens = response;
+                    api_tokens.renew_on = moment().add(response.expires_in, 'seconds');
+
+                    axios({
+                        method: method,
+                        url: url,
+                        data: data,
+                        headers: {
+                            'authorization': 'Bearer '.concat(api_tokens.access_token),
+                            'content-type': 'application/json'
+                        }
+                    }).then(function (response) {
+                        resolve(response.data);
+                    }).catch(function (error) {
+                        reject(error);
+                    });
+                })
+                .catch(function (error) {
+                    reject(error);
+                });
+        } else {
+            axios({
+                method: method,
+                url: url,
+                data: data,
+                headers: {
+                    'authorization': 'Bearer '.concat(api_tokens.access_token),
+                    'content-type': 'application/json'
+                }
+            }).then(function (response) {
+                resolve(response.data);
+            }).catch(function (error) {
+                reject(error);
+            });
+        }
+    });
 }
 
 var schedule = null;
-
-api_call('https://hoogstraaten.eu/schedule/1', 'get', null);
 
 // fs.readFile('/etc/systemd/system/RGBController/schedule.json', 'utf8', function (err,data) {
 //     if (err) {
@@ -132,9 +151,8 @@ function onchallenge (session, method, extra) {
 }
 
 function loadSchedule(id) {
-    axios.get('https://hoogstraaten.eu/schedule/' + id)
-        .then(function(response) {
-            //console.log(response.data);
+    api_call('https://hoogstraaten.eu/api/schedule/' + id, 'get', null)
+        .then(function (response) {
             schedule = response.data;
             fs.writeFile("/etc/systemd/system/RGBController/schedule.json", JSON.stringify(response.data), function(err) {
                 if(err) {
@@ -168,16 +186,17 @@ function loadSchedule(id) {
  * @param currenttime
  */
 function calculateSchedule(schedule, currenttime) {
-    var max = schedule.length - 1;
-    for (var index in schedule) {
+    console.log(schedule.entries);
+    var max = schedule.entries.length - 1;
+    for (var index in schedule.entries) {
         var index2 = parseInt(index) + 1;
-        var first = moment(schedule[index].time, 'HH:mm');
+        var first = moment(schedule.entries[index].time, 'HH:mm');
         //If last index then compare it with the first one
         if (index == max) {
             index2 = 0;
-            var second = moment(schedule[index2].time, 'HH:mm').add(1, 'days');
+            var second = moment(schedule.entries[index2].time, 'HH:mm').add(1, 'days');
         } else {
-            var second = moment(schedule[index2].time, 'HH:mm');
+            var second = moment(schedule.entries[index2].time, 'HH:mm');
         }
         //Get the two schedule entries where the current time is in between.
         if(currenttime.isBetween(first, second, 'minutes', '[)')){
@@ -189,24 +208,24 @@ function calculateSchedule(schedule, currenttime) {
             //console.log(first, second, maxdiff, currentdiff, percentage, schedule[index2]);
             //Calculate PWM level for each channel based on difference between level values of the two entries
             //and percentage of the progress
-            for (var i in schedule[index]) {
+            for (var i in schedule.entries[index].colors) {
                 if (i === 'time') {
                     continue;
                 }
-                if (parseInt(schedule[index][i]) !== parseInt(schedule[index2][i])) {
-                    var powerdiff =  parseInt(schedule[index2][i]) - parseInt(schedule[index][i]);
-                    var ledpower = parseInt(schedule[index][i]) + (Math.round(powerdiff * (percentage / 100)));
+                if (parseInt(schedule.entries[index].colors[i]) !== parseInt(schedule.entries[index2].colors[i])) {
+                    var powerdiff =  parseInt(schedule.entries[index2].colors[i]) - parseInt(schedule.entries[index].colors[i]);
+                    var ledpower = parseInt(schedule.entries[index].colors[i]) + (Math.round(powerdiff * (percentage / 100)));
                     if(pinvaulues[i + 'Pin'] !== ledpower) {
                         wpi.softPwmWrite(pins[i + 'Pin'], ledpower);
                         pinvaulues[i + 'Pin'] = ledpower;
                     }
                 } else {
-                    if(pinvaulues[i + 'Pin'] !== parseInt(schedule[index2][i])) {
-                        wpi.softPwmWrite(pins[i + 'Pin'], parseInt(schedule[index2][i]));
-                        pinvaulues[i + 'Pin'] = parseInt(schedule[index2][i]);
+                    if(pinvaulues[i + 'Pin'] !== parseInt(schedule.entries[index2].colors[i])) {
+                        wpi.softPwmWrite(pins[i + 'Pin'], parseInt(schedule.entries[index2].colors[i]));
+                        pinvaulues[i + 'Pin'] = parseInt(schedule.entries[index2].colors[i]);
                     }
                 }
-                console.log(schedule[index][i], ledpower, currenttime);
+                console.log(schedule.entries[index].colors[i], ledpower, currenttime);
             }
         }
     }
@@ -254,5 +273,5 @@ connection.onclose = function (reason, details) {
     console.log("Connection lost:", reason, details);
 }
 
-//loadSchedule();
+loadSchedule(1);
 connection.open();
